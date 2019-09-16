659c586c426fd26af205353c1b8bbee9431feb86
Yudai Watanabe <lock0308@gmail.com>
2019-09-16 11:51:25 +0900

    マイページをすべてコンポーネント化

--- resources/js/components/account/Account-deck.js
+++ resources/js/components/account/Account-deck.js
@@ -1,70 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {useState} from 'react';
 import {Row} from 'react-bootstrap';
 import AccountCard from './Account-card';
 import User from '../../User.js';
-import {axios} from "../../axios";
 
-class AccountDeck extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            titles: null,
-        }
-        // 一番最初に render() が呼ばれるタイミングでは titles はセットされてない。
-        // 同期処理が完了し、titles に値がセットされたタイミングで再度 render() が呼ばれる。
-    }
+function AccountDeck (props) {
+  const [titles, setTitles] = useState(props.titles);
+  const cards = (titles)
+    ? titles.map((title, i) => <AccountCard title={title} key={i} />)
+    : <p className="m-0 text-center">まずは新しい家計簿を作成しましょう！</p>
+  ;
 
-    componentDidMount () {
-        axios.get("/api/auth_user")
-             .then(res => {
-                 const user = res.data;
-                 axios.get(`/api/user/${user.id}/accounts`)
-                      .then(res => {
-                          const accounts = res.data;
-                          console.log(accounts)
-                          const titles = accounts.map(a => a.name);
-                          this.setState({titles: titles});
-                      })
-             })
-    }
-
-    // async getUser () {
-    //     const user = await axios.get("/api/auth_user");
-    //     return user.data;
-    // }
-
-    // async getMyAccounts (id) {
-    //     const url = `/api/user/${id}/accounts`;
-    //     const res = await axios.get(url);
-    //     return res.data;
-    // }
-
-    // async getTitles () {
-    //     const user = await this.getUser();
-    //     const accounts = await this.getMyAccounts(user.id);
-    //     const titles = accounts.map(a => a.name);
-    //     this.setState({titles: titles})
-    //     return;
-    // }
-
-    render () {
-        const titles = this.state.titles;
-        let cards;
-        if (titles) {
-            cards = titles.map((title, i) => {
-                return <AccountCard title={title} key={i} />
-            });
-        }
-
-        return (
-            <div className="bg-light rounded p-4">
-                <Row>
-                    {cards}
-                </Row>
-            </div>
-        )
-    }
+  return (
+    <div className="bg-light rounded p-4">
+      <Row>
+        {cards}
+      </Row>
+    </div>
+  );
 }
+
+// class AccountDeck extends React.Component {
+//     constructor (props) {
+//         super(props);
+//         this.state = {
+//             titles: null,
+//         }
+//         // 一番最初に render() が呼ばれるタイミングでは titles はセットされてない。
+//         // 同期処理が完了し、titles に値がセットされたタイミングで再度 render() が呼ばれる。
+//     }
+
+//     render () {
+//         const titles = this.state.titles;
+//         let cards;
+//         if (titles) {
+//             cards = titles.map((title, i) => {
+//                 return <AccountCard title={title} key={i} />
+//             });
+//         }
+
+//         return (
+//             <div className="bg-light rounded p-4">
+//                 <Row>
+//                     {cards}
+//                 </Row>
+//             </div>
+//         )
+//     }
+// }
 
 export default AccountDeck;
--- resources/js/components/mypage.js
+++ resources/js/components/mypage.js
@@ -1,19 +1,38 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
+import {axios} from "../axios";
 import {Container, Row, Col} from 'react-bootstrap';
 import AccountForm from './account/Account-form-modal';
 import AccountDeck from './account/Account-deck';
 
 function Mypage () {
+  const [titles, setTitles] = useState(null);
+
+  useEffect(() => {
+    const fetchTitles = async () =>{
+      let res;
+      res = await axios.get("/api/auth_user");
+      const user = res.data;
+      res = await axios.get(`/api/user/${user.id}/accounts`)
+      const accounts = res.data;
+      const titles = accounts.map(a => a.name);
+      setTitles(titles);
+    }
+    fetchTitles();
+  // 第２引数にこの副作用が依存している値の配列を渡すことができる。
+  // 空の配列を渡すとコンポーネント内のどの値にも依存してないことを示すことができる
+  // この場合、マウント時に実行、アンマウント時にクリーンアップされるが、アップデート時は実行されない。
+  }, []);
+
   return (
     <Container>
       <Row className="justify-content-center">
         <Col md="8">
           <div className="mb-4 border-bottom">
-            <p className="font-weight-bold">あなたとシェアされている家計簿</p>
+            <p className="font-weight-bold">あなたが作成した家計簿</p>
           </div>
-          <AccountForm/>
-          <AccountDeck/>
+          <AccountForm />
+          <AccountDeck titles={titles} />
         </Col>
       </Row>
     </Container>
