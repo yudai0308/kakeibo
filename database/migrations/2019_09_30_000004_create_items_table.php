70459004a00f68251d931ebd25a3ab1e910de3a6
Yudai Watanabe <lock0308@gmail.com>
2019-10-03 08:49:17 +0900

    マイグレーション追加、修正

--- database/migrations/2019_09_30_000004_create_items_table.php
+++ database/migrations/2019_09_30_000004_create_items_table.php
@@ -24,19 +24,19 @@ class CreateItemsTable extends Migration
         Schema::create($this->set_schema_table, function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->increments('id');
-            $table->unsignedInteger('account_id');
             $table->unsignedInteger('user_id')->nullable();
-            $table->unsignedInteger('category_id');
-            $table->string('name', 45);
+            $table->unsignedInteger('account_id');
+            $table->unsignedInteger('sub_category_id');
+            $table->string('memo', 45);
             $table->integer('amount');
             $table->date('date');
             $table->tinyInteger('isIncome');
 
             $table->index(["account_id"], 'fk_items_accounts1_idx');
 
-            $table->index(["category_id"], 'fk_items_categories1_idx');
-
             $table->index(["user_id"], 'fk_items_users1_idx');
+
+            $table->index(["sub_category_id"], 'fk_items_sub_categories1_idx');
 
             $table->unique(["id"], 'id_UNIQUE');
 
@@ -51,8 +51,8 @@ class CreateItemsTable extends Migration
                 ->onDelete('no action')
                 ->onUpdate('no action');
 
-            $table->foreign('category_id', 'fk_items_categories1_idx')
-                ->references('id')->on('categories')
+            $table->foreign('sub_category_id', 'fk_items_sub_categories1_idx')
+                ->references('id')->on('sub_categories')
                 ->onDelete('no action')
                 ->onUpdate('no action');
         });
@@ -63,8 +63,8 @@ class CreateItemsTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
-        Schema::dropIfExists($this->set_schema_table);
-    }
+     public function down()
+     {
+       Schema::dropIfExists($this->set_schema_table);
+     }
 }
--- database/migrations/2019_10_03_000003_create_sub_categories_table.php
+++ database/migrations/2019_10_03_000003_create_sub_categories_table.php
@@ -4,17 +4,17 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoriesTable extends Migration
+class CreateSubCategoriesTable extends Migration
 {
     /**
      * Schema table name to migrate
      * @var string
      */
-    public $set_schema_table = 'categories';
+    public $set_schema_table = 'sub_categories';
 
     /**
      * Run the migrations.
-     * @table categories
+     * @table sub_categories
      *
      * @return void
      */
@@ -24,9 +24,19 @@ class CreateCategoriesTable extends Migration
         Schema::create($this->set_schema_table, function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->increments('id');
+            $table->unsignedInteger('category_id');
             $table->string('name', 45);
+            $table->string('image');
+
+            $table->index(["category_id"], 'fk_sub_categories_categories1_idx');
 
             $table->unique(["id"], 'id_UNIQUE');
+
+
+            $table->foreign('category_id', 'fk_sub_categories_categories1_idx')
+                ->references('id')->on('categories')
+                ->onDelete('no action')
+                ->onUpdate('no action');
         });
     }
 
@@ -35,8 +45,8 @@ class CreateCategoriesTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
-        Schema::dropIfExists($this->set_schema_table);
-    }
+     public function down()
+     {
+       Schema::dropIfExists($this->set_schema_table);
+     }
 }
