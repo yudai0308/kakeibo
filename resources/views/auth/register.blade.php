7aa8dcb90000c6bd925ccc478c9b4b155742029c
Yudai Watanabe <lock0308@gmail.com>
2019-10-06 08:58:34 +0900

    表記修正

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,10 +12,10 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">ニックネーム</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">ユーザー名</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" placeholder="ニックネームを入力" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" placeholder="ユーザー名を入力" required autocomplete="name" autofocus>
 
                                 @error('name')
                                 <span class="invalid-feedback" role="alert">
