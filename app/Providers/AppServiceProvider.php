62b0ee52becd2dd68f9fbb45876d3b8cbeebf983
Yudai Watanabe <lock0308@gmail.com>
2019-10-06 20:44:45 +0900

    本番環境では https を強制する

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Schema::defaultStringLength(191);
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
