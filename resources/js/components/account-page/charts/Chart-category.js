3287d6dff4de338694412d83d8dcb112bbcabba7
Yudai Watanabe <lock0308@gmail.com>
2019-10-14 16:19:43 +0900

    円グラフ表示用の計算処理を分離

--- resources/js/components/account-page/charts/Chart-category.js
+++ resources/js/components/account-page/charts/Chart-category.js
@@ -2,76 +2,11 @@ import React from 'react';
 import {
   PieChart, Pie, Cell, Label, Legend, Tooltip, ResponsiveContainer,
 } from 'recharts';
-import { separate } from "../../libs";
+import {aggregateDataBy, displayExpensesTotal} from "../../item-libs";
 
 function CategoryChart({ items }) {
-  // グループ化されている items を配列に戻す
-  const alignItems = (items, key) => {
-    let alignedItems = [];
-    for (const k in items) {
-      const itemHashes = items[k]
-      for (let hash of itemHashes) {
-        hash[key] = k;
-        alignedItems.push(hash);
-      }
-    }
-    return alignedItems;
-  }
-  // 指定された key でグルーピングする
-  const groupBy = (items, key) => {
-    let groupedHash = {};
-    for (let itemHash of items) {
-      const group = itemHash[key];
-      const groups = Object.keys(groupedHash);
-      if (groups.includes(group)) {
-        groupedHash[group].push(itemHash);
-      } else {
-        groupedHash[group] = [itemHash];
-      }
-    }
-    return groupedHash;
-  }
-  // 指定したキーの値を合計する
-  const sumUpByKey = (items, key) => {
-    let sum = 0;
-    for (const item of items) {
-      sum += item[key];
-    }
-    return sum;
-  }
-  const aggregateDataBy = key => {
-    const itemsArr = alignItems(items, "date");
-    const groupedItems = groupBy(itemsArr, key);
-    const aggregationData = [];
-    for (const groupName in groupedItems) {
-      const groupedItemsArr = groupedItems[groupName];
-      if (groupedItemsArr[0].isIncome) continue;
-      const sum = sumUpByKey(groupedItemsArr, "amount");
-      const data = {
-        name: groupName,
-        value: sum,
-      }
-      aggregationData.push(data);
-    }
-    return aggregationData;
-  }
-  const sumUpExpenses = items => {
-    const itemsArr = alignItems(items);
-    let sum = 0;
-    for (let item of itemsArr) {
-      if (item.isIncome) continue;
-      sum += item.amount;
-    }
-    return sum;
-  }
-  const displayExpensesTotal = items => {
-    const sum = sumUpExpenses(items);
-    const separated = separate(sum);
-    return `¥ ${separated}`;
-  }
-
   const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#9b59b6"];
-  const data = aggregateDataBy("sub_category");
+  const data = aggregateDataBy(items, "sub_category");
 
   return (
     <ResponsiveContainer width="100%" height={400}>
