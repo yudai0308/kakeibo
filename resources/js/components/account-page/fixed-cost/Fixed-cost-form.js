136139b48e31123f5a216dfee35dcb7f2a4a220c
Yudai Watanabe <lock0308@gmail.com>
2019-10-20 01:03:18 +0900

    バグ修正

--- resources/js/components/account-page/fixed-cost/Fixed-cost-form.js
+++ resources/js/components/account-page/fixed-cost/Fixed-cost-form.js
@@ -57,6 +57,7 @@ function FixedCostForm(props) {
   }
 
   const findDefaultCost = id => {
+    if (!item.length) return null;
     const formHash = makeFormHash(items, baseForms);
     for (const hash of formHash) {
       if (hash.id === id) return hash.amount;
